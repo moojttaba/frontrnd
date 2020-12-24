f34e0cf79e101d72aab215fceb603999f3123da9
test <test@gmail.com>
2020-12-24 16:28:43 +0330

    e

--- src/pages/shop.page.jsx
+++ src/pages/shop.page.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, lazy, Suspense } from "react";
+import {
+  //useEffect,
+  lazy,
+  Suspense,
+  Fragment,
+} from "react";
 import { connect } from "react-redux";
-import { Route } from "react-router-dom";
+//import { Route } from "react-router-dom";
 
 import { fetchCollectionsStart } from "../redux/shop/shop.actions";
-
-import CollectionPage from "./collection.page";
+//import CollectionPage from "./collection.page";
 import Spinner from "../components/spinner.component";
-
 const CollectionsOverview = lazy(() =>
   import("../components/collection-overview.component")
 );
-
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   // useEffect(() => {
@@ -19,23 +21,26 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
   // fetchCollectionsStart();
 
   return (
-    <React.Fragment>
+    // <React.Fragment>
+    //   <Suspense fallback={<Spinner />}>
+    //     <Route exact path={`${match.path}`} component=     {CollectionsOverview} />
+    //     <Route
+    //       path={`${match.path}/:collectionId`}
+    //       component={CollectionPage}
+    //     />
+    //   </Suspense>
+    // </React.Fragment>
+
+    <Fragment>
       <Suspense fallback={<Spinner />}>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPage}
-        />
+        <CollectionsOverview/>
       </Suspense>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
-
-
-  
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
